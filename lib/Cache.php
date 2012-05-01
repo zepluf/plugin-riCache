df24cc400af7d1549c155f98b418f94674017ff5
yellow1912 <yellow1912@gmail.com>
2012-05-01 13:03:39 +0700

    moved the method to find relative path to utility

--- lib/Cache.php
+++ lib/Cache.php
@@ -104,35 +104,6 @@ class Cache {
 	public function exists($name, $cache_folder = '', $use_subfolder = false){
 		$cache_folder = $this->calculatePath($cache_folder, $use_subfolder);
 		return file_exists("$cache_folder/$name") ? "$cache_folder/$name" : false;
-	}
-	
-	function getRelativePath($from, $to)
-	{
-		$from = explode('/', $from);
-		$to = explode('/', $to);
-		foreach($from as $depth => $dir)
-		{
-
-			if(isset($to[$depth]))
-			{
-				if($dir === $to[$depth])
-				{
-					unset($to[$depth]);
-					unset($from[$depth]);
-				}
-				else
-				{
-					break;
-				}
-			}
-		}
-		//$rawresult = implode('/', $to);
-		for($i=0;$i<count($from)-1;$i++)
-		{
-			array_unshift($to,'..');
-		}
-		$result = implode('/', $to);
-		return $result;
 	}		
 	
 	private function calculatePath($cache_folder, $use_subfolder){
