a4ecb5495951db9706dcb61a7d055afa3382b967
yellow1912 <yellow1912@gmail.com>
2012-05-07 12:40:39 +0700

    multi bug fixes

--- lib/Cache.php
+++ lib/Cache.php
@@ -16,7 +16,7 @@ class Cache {
 
 	public function __construct(){
 	    $this->path = DIR_FS_CATALOG . Plugin::get('riPlugin.Settings')->get('riCache.path');
-	    $this->status = DIR_FS_CATALOG . Plugin::get('riPlugin.Settings')->get('riCache.status');
+	    $this->status = Plugin::get('riPlugin.Settings')->get('riCache.status');
 	}
 	
 	public function write($name, $cache_folder, $content, $use_subfolder = false){
@@ -29,11 +29,22 @@ class Cache {
 		
 		$cache_file = "$cache_folder/$name";
 		
-		$write = @file_put_contents($cache_file, $content);
-		
-		@chmod($cache_file, 0777);
-		
-		return $write ? $cache_file : false;
+		$written = 0;
+	    if ($fp = @fopen($cache_file, 'xb')) {
+
+	        // lock file for writing
+			if (flock($fp, LOCK_EX)) {
+				$written = fwrite($fp, $content);
+			}
+			fclose($fp);
+
+			// Set filemtime
+			touch($cache_file, time() + 3600);
+			
+			//@chmod($cache_file, 0777);			
+		}		
+						
+		return $written !== false && $written > 0 ? $cache_file : false;
 	}
 
 	public function read($name, $cache_folder ='', $use_subfolder = false){
@@ -52,8 +63,14 @@ class Cache {
 		return $read ? $read : false;
 	}
 	
+	public function remove($dir, $DeleteMe = false){
+	    $counter = 0;
+	    Plugin::get('riUtility.File')->sureRemoveDir($this->path . $dir, $DeleteMe, $counter);
+	    return $counter;
+	}
+	
 	public function startBlock($id, $change_on_page = false, $depend_on = "", $post_safe = true){
-		if(!$this->status || ($post_safe && $_SERVER["REQUEST_METHOD"] == 'POST') || isset($_GET[zen_session_id()]))
+		if(!$this->status || (!$post_safe && $_SERVER["REQUEST_METHOD"] == 'POST') || isset($_GET[zen_session_id()]))
 			return false;
                  
 		if($change_on_page) $id .=  getenv('REQUEST_URI');
@@ -104,7 +121,7 @@ class Cache {
 	public function exists($name, $cache_folder = '', $use_subfolder = false){
 		$cache_folder = $this->calculatePath($cache_folder, $use_subfolder);
 		return file_exists("$cache_folder/$name") ? "$cache_folder/$name" : false;
-	}		
+	}					
 	
 	private function calculatePath($cache_folder, $use_subfolder){
 	    $cache_folder = $this->path."$cache_folder/";
